fe2f6d4f1f82264fea345ffa8bc6ed57e72a03b4
Daniel Lehre Ekeberg <154937462+danielekeberg@users.noreply.github.com>
2025-03-12 06:28:41 +0100

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,21 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
   {
     languageOptions: {
-      globals:  {
+      globals: {
         ...globals.browser,
         module: true,
         require: true,
         process: true,
-        test: true
+        test: true,
       },
     },
     rules: {
-      "no-unused-vars": ["error", { "args": "all" }],
+      'no-unused-vars': ['error', { args: 'all' }],
     },
   },
 ];
